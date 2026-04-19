b1287f1c05ba5792df1e6d7f28ab96b93217f0e7
cr-ash-dummy <anewton3@terpmail.umd.edu>
2026-04-18 21:31:00 -0400

    Update finalproj.R

--- project/finalproj.R
+++ project/finalproj.R
@@ -1,3 +1,8 @@
+### I made two scatterplots just mapping every film by watch count and rating, and then by likes and rating. 
+### I'm probably going to have to cut the data down; the x-axis can't even be measured in actual numbers.
+### I tried to do some correlation tests, but they came back with NA. Does that mean there's no correlation, or that I did it wrong?
+### I should also make some histograms measuring average watch count and average-average rating.
+
 library(tidyverse)
 library(readr)
 Movie_Data_File <- read_csv("Movie_Data_File.csv")
